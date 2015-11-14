8b2ae9b72586df0a72b6ed2916a69fa5c166ede5
Dennis Lange <herr.dennis.lange@gmail.com>
2015-11-14 15:14:40 +0100

    finished

--- src/app.js
+++ src/app.js
@@ -1,17 +1,90 @@
+const _ = require('lodash');
+
+console.reset = function () {
+  return process.stdout.write('\033c');
+}
+
+var grid = generateRandomGrid(50, 100);
+console.reset();
+
+start(grid);
+function start(grid) {
+	setTimeout(function() {
+	console.reset();
+	grid = iterate(grid)
+	render(grid);
+	start(grid);
+}, 500)
+}
+
+
+
+
 module.exports = {
-	isAlive: function(grid, x, y) {
-		var broCount = 0;
-		for(var offx = -1; offx <= 1; offx++) {
-			for(var offy = -1; offy <= 1; offy++) {
-				if(grid[x + offx][y + offy]) {
-					broCount++;
-				}
-			}
-		}
-
-		broCount = grid[x, y] ? broCount - 1 : broCount;
-
-		console.log(broCount);
-		return !(broCount < 2) || (broCount === 2 || broCount === 3);
-	}
+	stayAlive: stayAlive,
+	countBros: countBros,
+	extractBros: extractBros,
+	generateRandomGrid: generateRandomGrid,
+	render: render
 };
+function stayAlive(grid, x, y) {
+		const count = countBros(grid, x, y);
+		if(grid[x][y]) {
+		  return count === 2 || count === 3;
+		}
+		return count === 3;
+		
+}
+
+function extractBros(grid, x, y) {
+	return _.range(-1, 2).map(function(offsetX) {
+		return _.range(-1, 2).map(function(offsetY) {
+			return _.get(grid,[x + offsetX, y + offsetY], 0);
+		}) 
+	});
+
+}
+function countBros(grid, x, y) {
+		const count = _.chain(extractBros(grid, x, y))
+		.flatten()
+		.sum()
+		.value();
+
+		return count - grid[x][y];
+};
+
+function generateRandomGrid(width, height) {
+	return _.range(0, width).map(function(x) {
+		return _.range(0, height).map(function(y) {
+			return Math.round(Math.random());
+		})
+	})
+}
+
+function iterate(grid) {
+	return mapGrid(grid, function(x, y) {
+		return stayAlive(grid, x, y) ? 1 : 0;
+	});
+}
+
+function toASCII(grid) {
+	return mapGrid(grid, function(x, y) {
+		return grid[x][y] === 1 ? 'x' : ' ';
+	});
+}
+
+function mapGrid(grid, fn) {
+	return _.range(0, grid.length).map(function(x) {
+		return _.range(0, grid[0].length).map(function(y) {
+			return fn(x, y);
+		})
+	})
+}
+
+function render(grid) {
+	grid.forEach(function(row) {
+		console.log(row.map(function(cell) {
+			return cell === 1 ? '█' : ' ';
+		}).join(''));
+	})
+}
--- test/test.js
+++ test/test.js
@@ -2,17 +2,158 @@ const
   expect = require('chai').expect,
   app = require(__dirname + '/../src/app.js');
 
-describe('livingCellWithNoBros', function() {
-  it('dies', function() {
-  	const noBros = [[false, false, false], [false, true, false], [false, false, false]];
-    expect(app.isAlive(noBros, 1, 1)).to.be.false;
+describe('in 3x3 Grid cell is Living', function() {
+  it('dies without bros', function() {
+    const noBros = [
+      [1, 1, 1, 1],
+      [1, 0, 0, 0],
+      [1, 0, 1, 0],
+      [1, 0, 0, 0],
+    ];
+
+    expect(app.stayAlive(noBros, 2, 2)).to.be.false;
+  });
+
+  it('lives on with 2', function() {
+    const noBros = [
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 0, 0]
+    ];
+
+    expect(app.stayAlive(noBros, 1, 1)).to.be.true;
+  });
+
+  it('lives on with 3', function() {
+    const noBros = [
+      [1, 1, 1],
+      [0, 1, 0],
+      [0, 0, 0]
+    ];
+
+    expect(app.stayAlive(noBros, 1, 1)).to.be.true;
+  });
+
+  it('dies with 4', function() {
+    const noBros = [
+      [1, 1, 1],
+      [0, 1, 1],
+      [0, 0, 0]
+    ];
+
+    expect(app.stayAlive(noBros, 1, 1)).to.be.false;
   });
 });
 
+describe('in 3x3 Grid cell is dead', function() {
+  it('should stay dead with 2 bros', function() {
+    const noBros = [
+      [1, 1, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
 
-describe('livingCellWithNoBros', function() {
-  it('dies', function() {
-  	const noBros = [[false, false, false], [true, true, true], [false, false, false]];
-    expect(app.isAlive(noBros, 1, 1)).to.be.true;
+    expect(app.stayAlive(noBros, 1, 1)).to.be.false;
+  });
+
+  it('should stay dead with 4 bros', function() {
+    const noBros = [
+      [1, 1, 1],
+      [0, 0, 1],
+      [0, 0, 0]
+    ];
+
+    expect(app.stayAlive(noBros, 1, 1)).to.be.false;
+  });
+  it('should come alive with 3 bros', function() {
+    const noBros = [
+      [1, 1, 1],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+
+    expect(app.stayAlive(noBros, 1, 1)).to.be.true;
   });
 });
+
+describe('extract bros', function() {
+  it('should extract bros', function() {
+    const env = [
+      [1, 1, 1, 1],
+      [1, 0, 0, 0],
+      [1, 0, 1, 0],
+      [1, 0, 0, 0],
+    ];
+
+    const expected = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ];
+
+    expect(app.extractBros(env, 2, 2)).to.deep.equal(expected);
+
+  });
+
+  it('should set out of range cells to 0', function() {
+  	const expected = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ];
+
+    const env = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 1],
+    ];
+
+    
+  })
+});
+
+describe('count bros', function() {
+  it('should have 0 bros', function() {
+    const noBros = [
+      [1, 1, 1, 1],
+      [1, 0, 0, 0],
+      [1, 0, 1, 0],
+      [1, 0, 0, 0],
+    ];
+    expect(app.countBros(noBros, 2, 2)).to.equal(0);
+  });
+
+  it('should have 3 bros', function() {
+    const threeBros = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [1, 1, 1]
+    ];
+    expect(app.countBros(threeBros, 1, 1)).to.equal(3);
+  });
+
+  it('should have 6 bros', function() {
+    const sixBros = [
+      [1, 1, 1],
+      [0, 1, 0],
+      [1, 1, 1]
+    ];
+    expect(app.countBros(sixBros, 1, 1)).to.equal(6);
+
+  });
+
+  describe('Generate Grid', function() {
+    it('should have 10 x 10 and only 1 or 0 values', function() {
+      var grid = app.generateRandomGrid(10, 10);
+      expect(grid.length).to.equal(10);
+      expect(grid[0].length).to.equal(10);
+
+      grid.forEach(function(row) {
+        row.forEach(function(cell) {
+          expect(cell === 1 || cell === 0).to.be.true;
+        });
+      });
+    });
+  });
+
+})
